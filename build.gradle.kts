f94a9e1c67c59a07846e1f3d1d14e39cd2f76248
Sylvain Bertrand <sbertrand@ihmc.org>
2022-06-30 15:19:18 -0500

    :bookmark: 17-0.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "17-0.1.2"
+   version = "17-0.1.3"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-javafx-extensions"
    openSource = true
    
