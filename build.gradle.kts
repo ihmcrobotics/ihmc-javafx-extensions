7b7512aef67f734d66c7bd2f6282b6ed741ceeea
smccrory <smccrory@ihmc.us>
2023-05-02 10:48:35 -0500

    :bookmark: 17-0.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "17-0.2.0"
+   version = "17-0.2.1"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-javafx-extensions"
    openSource = true
    
