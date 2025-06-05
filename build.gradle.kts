d0d9979ab66501f040afd6457e6f51673a1de5c4
Beomyeong <bpark@ihmc.org>
2025-06-05 13:56:44 -0500

    :arrow_up: euclid 0.22.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,6 @@ mainDependencies {
    api(ihmc.javaFXModule("controls", javaFXVersion))
    api(ihmc.javaFXModule("graphics", javaFXVersion))
    
-   api("us.ihmc:euclid:0.22.3")
+   api("us.ihmc:euclid:0.22.5")
 }
 
